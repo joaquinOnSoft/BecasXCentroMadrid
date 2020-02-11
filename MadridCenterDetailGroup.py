2e15ec193b51bc82894e036257934b2573731131
JoaquinOnSoft <joaquingp@gmail.com>
2020-02-11 19:14:33 +0100

    Improved help messages. Extra csv information files

--- MadridCenterDetailGroup.py
+++ MadridCenterDetailGroup.py
@@ -4,6 +4,16 @@ import sys
 from schoolarshipxcenter.consolidation.MadridCenterConsolidator import MadridCenterConsolidator
 
 
+def print_help():
+    print('Preparation of data to analyze the distribution of scholarships by Center in the Community of Madrid')
+    print('\nExecution:')
+    print('\tMadridCenterDetailGroup.py -i <input_file> -o <output_file>')
+    print('where:')
+    print('\t-h: Print this help')
+    print('\t-i: (Mandatory) input file (csv with a list of Centers (Schools, High schools...')
+    print('\t-o: (Mandatory) output file (csv file which will contains extended information for each center')
+
+
 def main(argv):
     input_file = None
     output_file = None
@@ -11,11 +21,12 @@ def main(argv):
     try:
         opts, args = getopt.getopt(argv, "hi:o:", ["input=", "output="])
     except getopt.GetoptError:
-        print('MadridCenterDetailGroup.py -i <input_file>')
+        print_help()
         sys.exit(2)
+
     for opt, arg in opts:
         if opt == '-h':
-            print('MadridCenterDetailGroup.py -i <input_file> -o <output_file>')
+            print_help()
             sys.exit()
         elif opt in ("-i", "--input"):
             input_file = arg
@@ -24,6 +35,8 @@ def main(argv):
 
     if input_file is not None and output_file is not None:
         MadridCenterConsolidator.process(input_file, output_file, ";")
+    else:
+        print_help()
 
 
 if __name__ == "__main__":
