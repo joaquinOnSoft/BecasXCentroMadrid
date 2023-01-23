777981ea6bfc05d90dd9287842190648db7b0a44
JoaquinOnSoft <joaquingp@gmail.com>
2023-01-23 23:31:27 +0100

    Grant consolidator

--- schoolarshipxcenter/consolidation/MadridGrantByCenterConsolidator.py
+++ schoolarshipxcenter/consolidation/MadridGrantByCenterConsolidator.py
@@ -1,4 +1,7 @@
+import csv
+
 from schoolarshipxcenter.reader.CSVReader import CSVReader
+from schoolarshipxcenter.writer.CSVWriter import CSVWriter
 
 
 class MadridGrantByCenterConsolidator:
@@ -6,15 +9,33 @@ class MadridGrantByCenterConsolidator:
     FIELD_CENTER_ID = "CÓDIGO DE CENTRO"
 
     @staticmethod
-    def process(center_file, center_delimiter, grant_file, grant_delimiter, output_file):
+    def process(center_file, center_delimiter, grant_file, grant_delimiter, output_file, output_delimiter):
         center_reader = CSVReader(center_file, center_delimiter)
-        rows = center_reader.read()
+        centers = center_reader.read()
 
-        if rows is not None:
+        if centers is not None:
 
-            lines = []
             num_lines = 0
+            lines = []
 
-            for row in rows:
-                # Remove column 'None'. Returned by CSVReader. It's a useless column
-                del row[None]
+            grant_reader = CSVReader(grant_file, center_delimiter)
+            grants = grant_reader.read()
+
+            if grants is not None:
+                grant_dict = {}
+                for grant in grants:
+                    grant_dict[grant[MadridGrantByCenterConsolidator.FIELD_CENTER_ID]] = \
+                        grant[MadridGrantByCenterConsolidator.FIELD_GRANT_AMOUNT]
+
+                for center in centers:
+                    # Remove column 'None'. Returned by CSVReader. It's a useless column
+                    del center[None]
+
+                    amount = grant_dict[grant[MadridGrantByCenterConsolidator.FIELD_CENTER_ID]]
+                    if amount is not None:
+                        amount = 0
+                    else:
+                        center[MadridGrantByCenterConsolidator.FIELD_GRANT_AMOUNT] = amount
+
+                reader = CSVWriter(output_file, output_delimiter, csv.QUOTE_ALL)
+                reader.write(centers)
