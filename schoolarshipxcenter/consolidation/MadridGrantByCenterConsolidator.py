7411419c3422342a3b1f42c86164187685fc7bfb
JoaquinOnSoft <joaquingp@gmail.com>
2023-01-25 23:17:07 +0100

    Fix bug crossing data from 2 excels

--- schoolarshipxcenter/consolidation/MadridGrantByCenterConsolidator.py
+++ schoolarshipxcenter/consolidation/MadridGrantByCenterConsolidator.py
@@ -5,8 +5,9 @@ from schoolarshipxcenter.writer.CSVWriter import CSVWriter
 
 
 class MadridGrantByCenterConsolidator:
-    FIELD_GRANT_AMOUNT = "CUANTÍA DE LAS BECAS"
-    FIELD_CENTER_ID = "CÓDIGO DE CENTRO"
+    FIELD_GR_GRANT_AMOUNT = "CUANTÍA DE LAS BECAS"
+    FIELD_GR_CENTER_ID = "CÓDIGO DE CENTRO"
+    FIELD_CE_CENTER = "CODIGO CENTRO"
 
     @staticmethod
     def process(center_file, center_delimiter, grant_file, grant_delimiter, output_file, output_delimiter):
@@ -24,18 +25,23 @@ class MadridGrantByCenterConsolidator:
             if grants is not None:
                 grant_dict = {}
                 for grant in grants:
-                    grant_dict[grant[MadridGrantByCenterConsolidator.FIELD_CENTER_ID]] = \
-                        grant[MadridGrantByCenterConsolidator.FIELD_GRANT_AMOUNT]
+                    grant_dict[grant[MadridGrantByCenterConsolidator.FIELD_GR_CENTER_ID]] = \
+                        grant[MadridGrantByCenterConsolidator.FIELD_GR_GRANT_AMOUNT]
 
+                center_id = None
                 for center in centers:
                     # Remove column 'None'. Returned by CSVReader. It's a useless column
                     del center[None]
 
-                    amount = grant_dict[grant[MadridGrantByCenterConsolidator.FIELD_CENTER_ID]]
-                    if amount is None:
+                    center_id = center[MadridGrantByCenterConsolidator.FIELD_CE_CENTER]
+                    if center_id in grant_dict.keys():
+                        amount = grant_dict[center_id]
+                    else:
                         amount = 0
 
-                    center[MadridGrantByCenterConsolidator.FIELD_GRANT_AMOUNT] = amount
+                    center[MadridGrantByCenterConsolidator.FIELD_GR_GRANT_AMOUNT] = amount
+
+                    amount = None
 
                 reader = CSVWriter(output_file, output_delimiter, csv.QUOTE_ALL)
                 reader.write(centers)
